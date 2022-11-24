ce3fb76ae4781067e57e3b8c6f595139b58a3bb3
Didiloy <didiloy2001@gmail.com>
2022-11-24 13:53:23 +0100

    deleted eclipse file

--- src/world/Location.java
+++ src/world/Location.java
@@ -3,6 +3,7 @@ package world;
 import character.NPCharacter;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public abstract class Location {
     protected String name;
@@ -12,9 +13,24 @@ public abstract class Location {
 
     public Map<String, String> lore;
 
-    public Location(String name){
+    public Location(String name) {
         this.name = name;
         //TODO parse les fichier appelé par le nom de la planete pour avoir le lore;
+        this.lore = new HashMap<String, String>();
+        String fileName = "./resource/" + this.name + ".csv";
+        String line = "";
+        String splitBy = ",";
+        try {
+            //parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] lineSplit = line.split(splitBy);    // use comma as separator
+                this.lore.put();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public NPCharacter getCharacter() {
